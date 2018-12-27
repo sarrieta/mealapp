2943ccb0f8f79619a002571034e3678db05aeae0
Sergio X <sergio.arrieta.siyom@gmail.com>
2018-12-27 15:52:54 +0100

    Initial index page created
    Initial index page created

--- mealsite/mealapp/urls.py
+++ mealsite/mealapp/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
 
-from . import views
+
+from django.urls import path, include
+from mealapp import views
+from django.conf.urls import url
+
 
 urlpatterns = [
 	path('',views.index, name='index'),
--- mealsite/mealsite/urls.py
+++ mealsite/mealsite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('mealapp/', include('mealapp.urls')),
+    path('', include('mealapp.urls')),
     path('admin/', admin.site.urls),
 ]
