f16832c6940a89672cc9ae9f732ef778e9a5f975
Sergio X <sergio.arrieta.siyom@gmail.com>
2019-02-17 17:35:55 +0000

    Summary
    Added geometry library to base.html script src for the map.
    Added calcdistance function.
    Get user location.
    Center map on user location.
    Radius input  to display restaurants around using location within the limit.
    Listener to re loadmap upon radius change

--- mealsite/mealapp/static/scripts/map/scripts.js
+++ mealsite/mealapp/static/scripts/map/scripts.js
@@ -14,6 +14,11 @@ function getCookie(name) {
     return cookieValue;
 }
 
+function calcDistance (fromLat, fromLng, toLat, toLng) {
+      return google.maps.geometry.spherical.computeDistanceBetween(
+        new google.maps.LatLng(fromLat, fromLng), new google.maps.LatLng(toLat, toLng));
+   }
+
 
    window.onload = function () {
 
@@ -21,9 +26,8 @@ function getCookie(name) {
 
    }
 
+
    function getCoordinates (event){
-
-
                $.ajax({
                         type:'GET',
                         url:'/plotMap/',
@@ -32,43 +36,218 @@ function getCookie(name) {
 
                           LoadMap(markers);
    						}
-
                          });
-
 
             }
 
    function LoadMap(markers) {
-       var mapOptions = {
-           center: new google.maps.LatLng(51.5257454,-0.0371079),
-           zoom: 8,
-           mapTypeId: google.maps.MapTypeId.ROADMAP
-       };
-       var infoWindow = new google.maps.InfoWindow();
-       var latlngbounds = new google.maps.LatLngBounds();
-       var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-       for (var i = 0; i < markers.coordinates.length; i++) {
-           var data = markers.coordinates[i]
-           var myLatlng = new google.maps.LatLng(data.lat, data.long);
-           var marker = new google.maps.Marker({
-               position: myLatlng,
-               map: map,
-               title: data.name
+     if (navigator.geolocation) {
 
-           });
-           (function (marker, data) {
-               google.maps.event.addListener(marker, "click", function (e) {
-                   infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.name + "</div>");
-                   infoWindow.open(map, marker);
 
-               });
-           })(marker, data);
-           latlngbounds.extend(marker.position);
-       }
-       var bounds = new google.maps.LatLngBounds();
-       map.setCenter(latlngbounds.getCenter());
-       map.fitBounds(latlngbounds);
+       navigator.geolocation.getCurrentPosition(function(position) {
+
+         var pos = {
+           lat: position.coords.latitude,
+           lng: position.coords.longitude
+         };
+         var mapOptions = {
+             center: new google.maps.LatLng(pos.lat,pos.lng),
+             zoom: 13,
+             mapTypeId: google.maps.MapTypeId.ROADMAP
+         };
+         var infoWindow = new google.maps.InfoWindow();
+         var latlngbounds = new google.maps.LatLngBounds();
+         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+         val='limit'
+         radius = radioVal(val)
+         console.log(radius)
+
+
+         if (radius)
+         {
+
+           limit = radius;
+
+         }
+         else{
+         limit = 5;
+
+          }
+
+
+         for (var i = 0; i < markers.coordinates.length; i++) {
+             var data = markers.coordinates[i]
+             var myLatlng = new google.maps.LatLng(data.lat, data.long);
+
+
+            var radius = parseInt(limit, 10)*1000;
+            console.log(radius)
+            console.log('is the radios')
+
+            var d = calcDistance(data.lat,data.long,pos.lat,pos.lng)
+            console.log(d)
+            console.log('is the distance')
+
+            if (d<=radius){
+              console.log('within radius')
+
+              var marker = new google.maps.Marker({
+                  position: myLatlng,
+                  map: map,
+                  title: data.name
+
+              });
+              (function (marker, data) {
+                  google.maps.event.addListener(marker, "click", function (e) {
+                      infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.name + "</div>");
+                      infoWindow.open(map, marker);
+
+                  });
+              })(marker, data);
+              latlngbounds.extend(marker.position);
+
+
+            }
+
+
+         }
+
+         initMap(map)
+
+       });
+
+     }
+
+
+   }
+
+   $(document).ready(function() {
+    $('input:radio[name=limit]').change(function() {
+        if (this.value == '1') {
+              radius=1;
+            //  alert(radius)
+             getCoordinates(radius)
+        }
+        else if (this.value == '5') {
+              radius=5;
+              //alert(radius)
+             getCoordinates(radius)
+        }
+        else if (this.value == '10') {
+            radius=10;
+            //alert(radius)
+             getCoordinates(radius)
+        }
+        else {
+            radius=15;
+            //alert(radius)
+             getCoordinates(radius)
+        }
+
+
+    });
+});
+
+   function radioVal(val){
+
+     var radios = document.getElementsByName(val);
+         for (var i = 0, length = radios.length; i < length; i++) {
+             if (radios[i].checked) {
+
+                 radios = radios[i].value
+
+                 return radios;
+
+                 break;
+             }
+         }
+
+   }
+
+   function initMap(map) {
+
+     var directionsDisplay = new google.maps.DirectionsRenderer;
+     var directionsService = new google.maps.DirectionsService;
+
+     directionsDisplay.setMap(map);
+     directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+     var control = document.getElementById('floating-panel');
+     control.style.display = 'block';
+     map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+     var onChangeHandler = function() {
+       calculateAndDisplayRoute(directionsService, directionsDisplay);
+     };
+     //document.getElementById('start').addEventListener('change', onChangeHandler);
+     document.getElementById('end').addEventListener('change', onChangeHandler);
+   }
+
+
+   function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+       if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
+
+              var geocoder = new google.maps.Geocoder;
+
+              var latlng = {lat: parseFloat(pos.lat), lng: parseFloat(pos.lng)};
+
+              geocoder.geocode({'location': latlng}, function(results, status) {
+                if (status === 'OK') {
+                  if (results[0]) {
+                    //console.log('in in if')
+                    currenLoc= results[0].formatted_address;
+
+                      var start = currenLoc
+
+
+                      var end = document.getElementById('end').value;
+
+                      directionsService.route({
+                        origin: start,
+                        destination: end,
+                        travelMode: 'DRIVING'
+                      }, function(response, status) {
+                        if (status === 'OK') {
+                          directionsDisplay.setDirections(response);
+                        } else {
+                          window.alert('Directions request failed due to ' + status);
+                        }
+                      });
+
+                  } else {
+                    window.alert('No results found');
+                  }
+                } else {
+                  window.alert('Geocoder failed due to: ' + status);
+                }
+              });
+
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            });
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
+
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(browserHasGeolocation ?
+                                'Error: The Geolocation service failed.' :
+                                'Error: Your browser doesn\'t support geolocation.');
+          infoWindow.open(map);
+        }
+
+
    }
    $(".reset").click( function getDirections(event){
 
@@ -76,52 +255,7 @@ function getCookie(name) {
 
    })
 
-    $(".view_route").click( function getDirections(event){
 
-
-      initMap()
-
-    })
-
-          function initMap() {
-
-            var directionsDisplay = new google.maps.DirectionsRenderer;
-            var directionsService = new google.maps.DirectionsService;
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 13,
-              center: {lat: 51.5257454, lng: -0.0371079}
-            });
-            directionsDisplay.setMap(map);
-            directionsDisplay.setPanel(document.getElementById('right-panel'));
-
-            var control = document.getElementById('floating-panel');
-            control.style.display = 'block';
-            map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-
-            var onChangeHandler = function() {
-              calculateAndDisplayRoute(directionsService, directionsDisplay);
-            };
-            document.getElementById('start').addEventListener('change', onChangeHandler);
-            document.getElementById('end').addEventListener('change', onChangeHandler);
-          }
-
-          function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-            var start = document.getElementById('start').value;
-
-            var end = document.getElementById('end').value;
-
-            directionsService.route({
-              origin: start,
-              destination: end,
-              travelMode: 'DRIVING'
-            }, function(response, status) {
-              if (status === 'OK') {
-                directionsDisplay.setDirections(response);
-              } else {
-                window.alert('Directions request failed due to ' + status);
-              }
-            });
-          }
 
 
 
@@ -172,15 +306,7 @@ $(document).ready(function(event){
           }
 
 
-          var radios = document.getElementsByName('food_type');
-          for (var i = 0, length = radios.length; i < length; i++) {
-              if (radios[i].checked) {
-
-                  radios = radios[i].value
-
-                  break;
-              }
-          }
+          var radios =radioVal('food_type')
 
 
           var csrftoken=getCookie('csrftoken')
