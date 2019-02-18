fb3a54ca3248cfd08e064e199112defe65b23ab2
Sergio X <sergio.arrieta.siyom@gmail.com>
2019-02-18 23:16:27 +0000

    Summary
    Created Index 2

--- mealsite/mealapp/urls.py
+++ mealsite/mealapp/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import url
 
 urlpatterns = [
 	path('',views.index, name='index'),
+	path('index2',views.index2, name='index2'),
 	path('map/',views.map, name='map'),
 	path('register/',views.register, name='register'),
 	path('login/',views.login, name='login'),
--- mealsite/mealapp/views.py
+++ mealsite/mealapp/views.py
@@ -132,6 +132,10 @@ def index(request):
 
     return render (request,'index.html')
 
+def index2(request):
+
+    return render (request,'index2.html')
+
 def profile(request):
 
 
